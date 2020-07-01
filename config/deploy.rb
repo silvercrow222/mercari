df11eda59860f7d1f70e0ee0a0c49e63bbb7a9dd
akioyokota <y.akio1910@gmail.com>
2020-07-01 16:37:54 +0900

    deploy3

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@
 lock "~> 3.14.1"
 
 set :application, "mercari"
-set :repo_url, "git@example.com:akioyokota/mercari.git"
+set :repo_url, "git@github.com:akioyokota/mercari.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.6.5'
 
 # chat-spaceで使ったpemを指定
 set :ssh_options, auth_methods: ['publickey'],
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,1 @@
-server '自身のElasticIP', user: 'ec2-user', roles: %w{app db web}
+server '52.192.240.140', user: 'ec2-user', roles: %w{app db web}
