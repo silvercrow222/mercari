0642647c9f29f9d66c89061310a59765e07e97a1
pony-pony <k.kenta1252@gmail.com>
2020-07-08 17:08:38 +0900

    add form

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_07_08_064039) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "src"
+    t.string "src", null: false
     t.bigint "product_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :product do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+  end
+
+end
