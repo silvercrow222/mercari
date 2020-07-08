3ebd469202099c25fa65e923346082f009aa044d
shin-iwa <luckystar0443@gmail.com>
2020-07-08 09:57:28 +0900

    adding validation of icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   VALID_PHONE = /\A\d{10}$|^\d{11}\z/
   VALID_KATAKANA = /\A[\p{katakana}\p{blank}ー－]+\z/
 
-  validates :family_name, :first_name, :birthday, :nickname, :encrypted_password, :icon, presence: true
+  validates :family_name, :first_name, :birthday, :nickname, :encrypted_password, presence: true
   validates :profile, presence: true, allow_blank: true
+  validates :icon, presence: true, allow_blank: true
   validates :email, {presence: true, format: { with: VALID_EMAIL }, uniqueness: { case_sensitive: false }}
   validates :phone, {presence: true, format: { with: VALID_PHONE }, uniqueness: { case_sensitive: false }}
   validates :family_name_kana, presence: true, format: { with: VALID_KATAKANA, message: 'はカタカナで入力して下さい'}
