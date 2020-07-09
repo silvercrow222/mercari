39cd24ed0be6da8333c5d509c6939e53927cb68e
pony-pony <k.kenta1252@gmail.com>
2020-07-09 10:54:04 +0900

    Update product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,67 +25,67 @@ describe Product do
     it "is invalid without a name" do
       product = build(:product, name: "")
       product.valid?
-      expect(product.errors[:name]).to include("can't be blank")
+      expect(product.errors[:name]).to include("を入力してください")
     end
 
     it "is invalid without a detail" do
       product = build(:product, detail: "")
       product.valid?
-      expect(product.errors[:detail]).to include("can't be blank")
+      expect(product.errors[:detail]).to include("を入力してください")
     end
 
     it "is invalid without a price" do
       product = build(:product, price: nil)
       product.valid?
-      expect(product.errors[:price]).to include("can't be blank")
+      expect(product.errors[:price]).to include("を入力してください")
     end
 
     it "is invalid without a condition" do
       product = build(:product, condition: nil)
       product.valid?
-      expect(product.errors[:condition]).to include("can't be blank")
+      expect(product.errors[:condition]).to include("を入力してください")
     end
 
     it "is invalid without a day" do
       product = build(:product, day: nil)
       product.valid?
-      expect(product.errors[:day]).to include("can't be blank")
+      expect(product.errors[:day]).to include("を入力してください")
     end
 
     it "is invalid without a shipping" do
       product = build(:product, shipping: nil)
       product.valid?
-      expect(product.errors[:shipping]).to include("can't be blank")
+      expect(product.errors[:shipping]).to include("を入力してください")
     end
 
     it "is invalid without a fee" do
       product = build(:product, fee: nil)
       product.valid?
-      expect(product.errors[:fee]).to include("can't be blank")
+      expect(product.errors[:fee]).to include("を入力してください")
     end
 
     it "is invalid without a prefecture_id" do
       product = build(:product, prefecture_id: nil)
       product.valid?
-      expect(product.errors[:prefecture_id]).to include("can't be blank")
+      expect(product.errors[:prefecture_id]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
       product = build(:product, user_id: nil)
       product.valid?
-      expect(product.errors[:user_id]).to include("can't be blank")
+      expect(product.errors[:user_id]).to include("を入力してください")
     end
 
     it "is invalid when price is less than 300" do
       product = build(:product, price: 100)
       product.valid?
-      expect(product.errors[:price]).to include("must be greater than or equal to 300")
+      expect(product.errors[:price]).to include("は300以上の値にしてください")
     end
 
     it "is invalid when price is more than 9999999" do
       product = build(:product, price: 10000000)
       product.valid?
-      expect(product.errors[:price]).to include("must be less than or equal to 9999999")
+      expect(product.errors[:price]).to include("は9999999以下の値にしてください")
     end
   end
 end
