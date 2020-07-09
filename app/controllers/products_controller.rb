fd16c2f95b1731efa8fb4a392a37e075fbd97030
akioyokota <y.akio1910@gmail.com>
2020-07-09 21:21:39 +0900

    update4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
+
+  def show
+  end
   
   def new
     @product = Product.new
