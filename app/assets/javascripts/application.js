1d66592e2df49e34f45fe33f7d9fea4e09d568a3
silvercrow222 <silvercrow222@i.softbank.jp>
2020-07-21 22:44:46 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,3 @@
 //= require_tree ./modules
 //= require rails-ujs
 //= require activestorage
-//= require jquery_ujs
--- app/assets/javascripts/modules/productShow.js
+++ app/assets/javascripts/modules/productShow.js
@@ -1,20 +1,13 @@
 $(function() {
-  //画像にマウスを乗せたら発動
-  $('.photo').hover(function() {
-    $('.photo').addClass('photoActive');
-    $('.photoActive').removeClass('photo');
-  // //ここにはマウスを離したときの動作を記述
-  }, function() {
-    $('.photoActive').removeClass('photoActive');
-  });
-});
-$(function() {
-  //画像にマウスを乗せたら発動
-  $('.photoActive').hover(function() {
-    $('.photoActive').addClass('photo');
-    $('.photo').removeclass('photoActive');
-  // //ここにはマウスを離したときの動作を記述
-  }, function() {
-    $('.photo').removeClass('photo');
-  });
+  $('.ProductShow__images--image').hover(
+    function() {
+      if ($(this).hasClass('active')) {
+      } else {
+        $('.ProductShow__images--image').removeClass('active');
+        $(this).addClass('active');
+        $('.ProductShow__imageTop').attr({'src': $(this).attr('src')});
+      }
+    }, function() {
+    }
+  );
 });
