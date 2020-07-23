2f6cdd4542b8907e65e64a69e4ce10517a97e5d4
kiyofujikun <kypommg@gmail.com>
2020-07-23 18:43:50 +0900

    komito

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -8,8 +8,8 @@ class PurchaseController < ApplicationController
     if card.blank?
       redirect_to controller: "cards", action: :new
     else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-      # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
@@ -18,8 +18,8 @@ class PurchaseController < ApplicationController
   def pay
     card = Card.where(user_id: current_user.id).first
     @product = Product.find(params[:id])
-    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-    # Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
     :amount => @product.price,
     :customer => card.customer_id,
