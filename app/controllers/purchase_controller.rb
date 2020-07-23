29a6ce9dbced25a8c454ed8004e49a5e602013e6
kiyofujikun <kypommg@gmail.com>
2020-07-23 20:47:30 +0900

    updeta

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,16 +2,20 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def show
-    @address = Address.where(user_id: current_user.id).first
-    card = Card.where(user_id: current_user.id).first
-    @product = Product.find(params[:id])
-    if card.blank?
-      redirect_to controller: "cards", action: :new
+    - if user_signed_in?
+      @address = Address.where(user_id: current_user.id).first
+      card = Card.where(user_id: current_user.id).first
+      @product = Product.find(params[:id])
+      if card.blank?
+        redirect_to controller: "cards", action: :new
+      else
+        # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+      end
     else
-      # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      redirect_to new_user_session_path
     end
   end
 
